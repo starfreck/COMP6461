cda03911bf3809ea7d99ed23211f590873ad7604
StarFreck <vasuratanpara@gmail.com>
2021-09-29 10:17:06 -0400

    init

---
+++ A1/http_lib.py
@@ -0,0 +1,21 @@
+class http:
+    """This is the http class"""
+    headers = dict()
+    parameters = dict()
+
+
+    def __init__(self, headers, parameters, body):
+        self.body = body
+        self.headers = headers
+        self.parameters = parameters
+
+    def http_get(self):
+
+
+    def http_post(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
