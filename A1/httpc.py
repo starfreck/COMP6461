7bce20ffb84b18d72b4b797b57b00be810e07347
Yun Ni <ni.yun@outlook.com>
2021-10-01 17:11:21 -0400

    add -o

--- A1/httpc.py
+++ A1/httpc.py
@@ -88,6 +88,10 @@ def get_handler(argv):
         headers = argv[location]
         argv.remove(headers)
 
+    if '-o' in argv:
+        o_index = argv.index('-o')
+        o_path = argv[o_index + 1]
+
     if len(argv) >= 1:
         if Debug:
             print("\nverbose:", verbose, "headers:", headers, "url:", argv[0], "\n")
@@ -132,6 +136,10 @@ def post_handler(argv):
             argv.remove("-f")
             file = argv[location]
             argv.remove(file)
+
+        if '-o' in argv:
+            o_index = argv.index('-o')
+            o_path = argv[o_index + 1]
 
         if len(argv) >= 1:
             if Debug: print("\nverbose:", verbose, "headers:", headers, "url:", argv[0], "string:", string, "file:",
